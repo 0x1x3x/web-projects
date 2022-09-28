64aab71653c8f27a597878d46c4232290e2735d6
0x1x3x <0x1x3x@protonmail.com>
2022-09-28 17:41:15 +0200

    ver. 3.0

--- behind the meta/assets/js/mouse.js
+++
@@ -1,8 +0,0 @@
-document.body.addEventListener("mousemove", function () {
-  console.log(event.clientX);
-
-  var target = document.querySelector(".mouse--follower");
-
-  target.style.left = event.clientX + "px";
-  target.style.top = event.clientY + "px";
-});
