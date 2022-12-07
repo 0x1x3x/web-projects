f528e7db04a924ba7627a57b862796b22165f26f
Eugene <0x1x3x@protonmail.com>
2022-12-07 08:49:07 +0100

    ver.1.2

--- online-shop/src/components/Header.js
+++ online-shop/src/components/Header.js
@@ -8,6 +8,20 @@ function Header(props) {
           <p>Barcelona</p>
         </div>
       </div>
+      <nav className="d-flex">
+        <a href=" " className="mr-30">
+          Sneakers
+        </a>
+        <a href=" " className="mr-30">
+          Streetwear
+        </a>
+        <a href=" " className="mr-30">
+          Shop
+        </a>
+        <a href=" " className="mr-30">
+          Contacts
+        </a>
+      </nav>
       <ul className="d-flex">
         <li onClick={props.onClickCart} className="mr-30 cu-p">
           <img width={18} height={18} src="/img/cart.png" alt="" />
