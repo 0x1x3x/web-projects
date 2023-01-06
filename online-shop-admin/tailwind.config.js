8a7650aa03811fac38bcff3f91372cf2a041c145
Eugene <0x1x3x@protonmail.com>
2023-01-06 14:01:27 +0100

    tailwind

---
+++ online-shop-admin/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/AddItem.js",
+    "./src/Item.js",
+    "./src/ItemsList.js",
+    "./src/Shop.js",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
