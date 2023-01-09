3da1307d160c18654b6a8b41d69111ba3cbf9da6
Eugene <0x1x3x@protonmail.com>
2023-01-09 18:43:21 +0100

    mapbox-gl ver. 1.0

--- react-components/mapbox-gl/src/index.js
+++ react-components/mapbox-gl/src/index.js
@@ -1,29 +1,19 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
-import mapboxgl from "mapbox-gl";
 import "./index.css";
+import useMapbox from "./useMapbox.hook";
 
 function App() {
   const [random, setRandom] = useState(Math.random());
 
-  mapboxgl.accessToken =
-    "pk.eyJ1Ijoib3RyaWNhbGEiLCJhIjoiY2tqMjFjcTNzMnp2djJ1bW05NW9zd2h6NCJ9.0w535oE4G9JjDpDxC_vVuw";
-
-  useLayoutEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/dark-v10",
-      center: [37.61192, 55.76199],
-      zoom: 10,
-    });
-  }, []);
+  useMapbox();
 
   return (
     <>
       <button id="rerender" onClick={() => setRandom(Math.random())}>
         Ререндер!
       </button>
-      <div id="map"></div>
+      <div id="map" className="min-h-screen"></div>
     </>
   );
 }
